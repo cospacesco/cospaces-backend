ae6cc83066753c9a18dfaa03ac7a99d3127ca56b
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-12-14 18:26:37 +0530

    load port from config

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 'use strict';
 
+var config = require('./config');
 var loaders = require('./loaders');
 var app = require('express')();
 
 async function startServer() {
   await loaders.init(app);
   
-  var port = process.env.PORT || 10010;
-  
-  app.listen(port, () => {
-    console.log('\nServer is up & running at http://127.0.0.1:' + port + '/api...');
-    console.log('Read docs at http://127.0.0.1:' + port + '/docs');
+  app.listen(config.port, () => {
+    console.log('\nServer is up & running at http://127.0.0.1:' + config.port + '/api...');
+    console.log('Read docs at http://127.0.0.1:' + config.port + '/docs');
   });
 }
 
