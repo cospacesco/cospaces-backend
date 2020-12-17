bb4eade0e23cc27adc37315854c84cf51edda6e6
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-12-17 11:56:58 +0530

    use swagger-ui-express

--- loaders/express.js
+++ loaders/express.js
@@ -3,6 +3,14 @@ const api = require('../api');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./api/swagger/swagger.yaml');
+
+var swaggerUiOptions = {
+  customCss: 'div.swagger-ui > div { max-width:960px; margin:auto; } .swagger-ui .topbar { display: none }'
+};
+
 function initServer(expressApp) {
   console.log("\nInitializing Express Server...")
 
@@ -10,6 +18,9 @@ function initServer(expressApp) {
   expressApp.use(bodyParser.json());
   expressApp.use(config.api.prefix, api);
   
+  // Serve Swagger UI
+  expressApp.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerUiOptions));
+
   console.log("Express Server initialized")
 }
 
